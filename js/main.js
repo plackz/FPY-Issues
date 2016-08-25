615b7a4c98d008a6357509b841eb1469eddae48e
plackz <pzaffina@slb.com>
2016-08-24 23:41:21 -0500

    added the select functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+/* TODO: convert to FPY example  */
+
+/* store the result of the selct dropdown */
 var select = document.querySelector('select');
 
 
+var para = document.querySelector('p'); 
+
+select.addEventListener('change', submitChoice);
+
+function submitChoice() {
+  var choice = select.value;
+
+/* TODO: convert to sending data to db */
+
+  if(choice === 'reset') {
+    para.textContent = 'You chose reset.';
+  } else if(choice === 'frozen') {
+    para.textContent = 'You chose frozen.';
+  } else {
+    para.textContent = '';
+  }
+
+}
