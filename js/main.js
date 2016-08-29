0c046f8576f162b71d8aee1720495eb0585794d0
plackz <pzaffina@slb.com>
2016-08-29 16:29:57 -0500

    added changes to match hta

--- js/main.js
+++ js/main.js
@@ -1,24 +1,60 @@
-/* TODO: convert to FPY example  */
+/*
+ * Auto-generated content from the Brackets New Project extension.  Enjoy!
+ */
 
-/* store the result of the selct dropdown */
-var select = document.querySelector('select');
+' setting the window size and position. when changing set the screen.avail* offsets to match from resizeTo
+
+/*
+
+var myWindow = window.open("","","width=300, height=800");
+
+myWindow.resizeTo(300,800);
+windowLeft = (window.screen.availWidth - 300);
+windowTop = (window.screen.availHeight - 800);
+myWindow.moveTo(windowLeft, windowTop);
+
+*/
+
+/* Selection box  
+
+function selectDropDown() {
+
+    var issues = ["Issue 1", "Issue 2", "Issue 3"];
+    var select = document.getElementById("selectIssues");
 
 
-var para = document.querySelector('p'); 
-
-select.addEventListener('change', submitChoice);
-
-function submitChoice() {
-  var choice = select.value;
-
-/* TODO: convert to sending data to db */
-
-  if(choice === 'reset') {
-    para.textContent = 'You chose reset.';
-  } else if(choice === 'frozen') {
-    para.textContent = 'You chose frozen.';
-  } else {
-    para.textContent = '';
-  }
-
+    for ( i = 0; i < issues.length; i++) {
+        var options = document.createElement('option');
+        options.innerHTML = issues[i];
+        options.value = issues[i];
+        select.appendChild(options);
+    } 
 }
+
+*/
+
+/* Add Other issues
+TODO: convert from VBScript to Javascript
+    Sub addOther
+        Dim Input
+        Input = InputBox("Enter Issue")
+        
+        Const ForAppending = 8
+
+        Set objFSO = CreateObject("Scripting.FileSystemObject")
+
+        Set objFile = objFSO.OpenTextFile("\\okcpfs01vh\Shared\unsecured\QUALITY\Projects\FPY-Issue-Capture\FPYIssuesLog.txt", ForAppending)
+    
+        Set WshNetwork = CreateObject("WScript.Network")
+        
+        objFile.Write WshNetwork.ComputerName
+        
+        objFile.Write "; " & Input & "; " & Now & vbCrLf
+        
+        objFile.Close    
+        
+        MsgBox "Submitted '" & Input & "'. Thank you!"        
+        
+    End Sub
+
+    */
